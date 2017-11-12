5650b49c27b69031c8da1a4ddc1f3be9be28c1e9
Patience <patiencecode@gmail.com>
2017-11-12 22:29:47 +0100

    uncommented favicon

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ var app = express();
 mongoose.connect('mongodb://mygold:supperGold@ds249545.mlab.com:49545/project');
 // mongoose.connect('mongodb://127.0.0.1:27017/project');
 // view engine setup
-app.engine('ejs',engine)
+app.engine('ejs',engine);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
